4c03a4d6bfcc62c6212aef50354eeb850ec2a26c
lomasgabe <glomas9205@stu.neisd.net>
2023-08-30 12:07:11 -0700

    Update ExamplePython.py
    Password Deleted!

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,5 +1,4 @@
-def test_password(x):
-    print(x)
+
 
 a = "String 1"
 b = "String 2"
@@ -16,5 +15,3 @@ job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 for name, job in job_list.items():
     print(f"{name} is a {job}")
 
-    password = 'SuperSecretAdminPass'
-    test_password(password)
